27a21f9be14efb52c06231492f9cb5d8fb52ce24
Abhishek Bhadoriya <127028405+abhishekbhadoriya421@users.noreply.github.com>
2025-07-30 20:16:41 +0530

    Optamize Count Occurrences

--- Count Occurrences using Binary Search.py
+++ Count Occurrences using Binary Search.py
@@ -1,41 +1,49 @@
-class Array:
-    def __init__(self, arr,size):
-        self.arr = arr
-        self.size = size
-    
-    def binarySearch(self,target):
-        start = 0
-        last = self.size -1
-        countOcc = 0
-        while start <= last:
-            # Find Mid
-            # mid (3 , 6)  3 + (6-3)/2 => 3 + 3/2 => 3 + 1 => 4
-            # mid (0 , 6)  0 + (6-0)/2 => 0 + 6/2 => 0 + 3 => 3
-            # mid = int(start + (last - start) / 2)
-            mid = start + (last - start) // 2
-            
-            if self.arr[mid] == target:
-                ## first i will check all target values from left side
-                countOcc+=1
-                checkLeft = mid -1
-                while(checkLeft>=start and self.arr[checkLeft] == target):
-                    countOcc+=1
-                    checkLeft-=1
-                
-                ## check for right element and count all occurrences of target
-                checkRight = mid + 1
-                while (checkRight <= last and self.arr[checkRight] == target):
-                    countOcc+=1
-                    checkRight+=1
-                
-                return countOcc
-            elif self.arr[mid] > target:
-                last = mid - 1
-            else:
-                start = mid + 1
-        return 0   
-        
+def findFirst(arr,start,end,target):
+    while(start <= end):
+        mid = start + (end - start) // 2
+        if ((arr[mid] == target and mid == start)
+        or (mid > start and arr[mid] == target and arr[mid-1] != target)):
+            return mid
+        elif mid > start and arr[mid - 1] == target:
+            end = mid - 1
+        else :
+            start = mid + 1
+           
+def FindLast(arr,start,end,target):
+    while(start <= end):
+        mid = start + (end - start) // 2
+        if ((arr[mid] == target and mid == end)
+        or (mid < end and arr[mid] == target and arr[mid+1] != target)):
+            return mid
+        elif mid < end and arr[mid + 1] == target:
+            start = mid + 1
+        else :
+            end = mid - 1
 
-arr = [1, 2, 2, 2, 3, 4, 5]
-Obj = Array(arr,len(arr))
-print(Obj.binarySearch(5))
+def findMidElement(arr,size,target):
+    start = 0
+    end = size - 1
+    firstIndex = -1
+    endIndex = -1
+    while(start<=end):
+        mid = start + (end - start) // 2
+        if arr[mid] == target:
+            firstIndex = findFirst(arr,start,mid,target)
+            endIndex = FindLast(arr,mid,end,target)
+            return endIndex - firstIndex +1
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
+
+# arr = [1,2,3,4,5,6,7,8]
+# arr2 = [1,1,2,2,2,2,2,8]
+# arr2 = [1, 2, 2, 2, 3, 4]
+# target = 3
+# arr = [1, 2, 2, 3, 3, 3, 4]
+# print(findMidElement(arr, len(arr), 3))  # E
+
+arr = [1,1,3,4,5,5,5,5,5]
+print(findMidElement(arr, len(arr), 3))
+# print(findMidElement(arr2,len(arr2),3))
