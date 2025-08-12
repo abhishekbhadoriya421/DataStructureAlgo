7c18b630663102a762e593e7d84973fafcfbdd62
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-12 06:18:22 +0530

    find missing number

--- Missing in Array.py
+++ Missing in Array.py
@@ -9,11 +9,26 @@ def helper(arr,index):
 	return helper(arr,index+1)
 
 
+# def  findMissingNumber(arr):
+# 	arr.sort()
+# 	return helper(arr,0)
 
 
-def  findMissingNumber(arr):
-	arr.sort()
-	return helper(arr,0)
+def findMissingNumber2(arr):
+	totalSum = 0
+	for item in arr:
+		totalSum+=item
+
+	actualNum = 0
+	for item in range(1,max(arr)+1):
+		actualNum+=item
+
+	print(actualNum," ",totalSum)
+	if(actualNum - totalSum ==0):
+		return max(arr)+1
+	else:
+		return actualNum - totalSum
 
 
-print(findMissingNumber([8, 2, 4, 5, 3, 7, 1,6]))
+
+print(findMissingNumber2([1,2,3,4,5]))
