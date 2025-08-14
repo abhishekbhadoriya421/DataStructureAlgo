364b97863b3c2ac5b83ff857c3fb825a1841eff0
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-14 08:51:29 +0530

    optimized

--- Recursion/Fibonacci Number.py
+++ Recursion/Fibonacci Number.py
@@ -6,5 +6,19 @@ def fibonacci(num):
 
 	return fibonacci(num-1) + fibonacci(num-2)
 
+def optimizeFibo(num):
+	if num == 1:
+		return  0
+	if(num == 2):
+		return 1
 
-print(fibonacci(7))
+	previouse = 0
+	previouse2 = 1
+	ans = previouse + previouse2
+	for item in range(3,num+1):
+		ans = previouse + previouse2
+		previouse = previouse2
+		previouse2 =ans
+	return ans
+
+print(optimizeFibo(8))
