191757c38822d6b622c35fafac114a27d589763e
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-22 21:34:38 +0530

    update but need fix

--- Array/Remove Duplicates from Sorted Array.py
+++ Array/Remove Duplicates from Sorted Array.py
@@ -6,6 +6,26 @@ def removeDuplicate(array):
             result.append(array[i])
     return result
 
+def removeDup(array):
+    i = 1 # replace value
+    j = i +1
+    while(i<len(array)-1 and j <len(array)-1 ):
+        while( j < len(array)-1  and array[j] == array[j+1]):
+            j+=1
+        temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+        i+=1
+        j+=1
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+    return array
+    
+    
+        
 
-array = [0,0,1,1,1,2,3,3,4]
-print(removeDuplicate(array))
+
+array = [0,0,1,1,1,2,3,3,4,4,5]
+array = [0,1,2,3,4,5,5,6]
+print(removeDup(array))
