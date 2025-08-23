8c5f2d37aff51715ed9edec30fa049a7a9960c49
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-23 10:15:47 +0530

    optimize remove duplicate

--- Array/Remove Duplicates from Sorted Array.py
+++ Array/Remove Duplicates from Sorted Array.py
@@ -22,10 +22,29 @@ def removeDup(array):
     array[j] = temp
     return array
     
-    
-        
+
+def bestApproach(array):
+    i = 1
+    j = i+1
+    maxi = array[0]
+    while(j<len(array)):
+        if(array[i-1]<array[i]):
+            i+=1
+            j+=1
+        elif(array[i]<array[j] and maxi < array[j]):
+            maxi = array[j]
+            temp = array[i]
+            array[i] = array[j]
+            array[j] = temp
+            i+=1
+            j+=1
+        else:
+            j+=1
+    return array
 
 
-array = [0,0,1,1,1,2,3,3,4,4,5]
-array = [0,1,2,3,4,5,5,6]
-print(removeDup(array))
+# array = [0,0,1,1,1,2,3,3,4,4,5]
+# array = [0,1,2,3,3,4,5]
+array= [1,2,2,3]
+print(array)
+print(bestApproach(array))
