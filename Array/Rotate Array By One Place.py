f3cd0cb071d40636b4e0f9075089723e038cac7f
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-25 06:04:36 +0530

    rotate array by one place

---
+++ Array/Rotate Array By One Place.py
@@ -0,0 +1,15 @@
+def rotateArrayByOne(self, nums):
+        first = nums[0]
+        i = 0
+        j = i+1
+        while(j<len(nums)):
+            nums[i] = nums[j]
+            i+=1
+            j+=1
+        
+        nums[i] = first
+        
+
+array = [1,2,3,4,5]
+rotateArrayByOne(array)
+print(array)
