ea538762e037d2e8b3eb8f9ba41cdca393d2c787
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-25 06:53:52 +0530

    better approach but taking extra space

--- Array/Rotate Array by  d place.py
+++ Array/Rotate Array by  d place.py
@@ -1,3 +1,5 @@
+
+# Brute Force Approach
 def rotateArrayByDPlace(nums,k):
         while(k>0):
             i = 0
@@ -10,11 +12,34 @@ def rotateArrayByDPlace(nums,k):
             nums[i] = first
             k-=1
         
-        
 
-# array = [1,2,3,4,5]
+# Better Approach
+
+def rotateArrayByDPlaceBetter(array, k):
+        if len(array)<k:
+            k = k % len(array)
+        temp = [0]* k
+        i = 0
+        while(i<k):
+            temp[i] = array[i]
+            i+=1
+            
+        i = 0
+        j =  k
+        while(j<len(array)):
+            array[i] = array[j]
+            i+=1
+            j+=1
+        j=0
+        while(i<len(array)):
+            array[i] = temp[j]
+            i+=1 
+            j+=1
+    
+
+array = [1,2,3,4,5]
 # array = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-array = [7, 3, 9, 1]
-k = 9
-rotateArrayByDPlace(array,k)
+# array = [7, 3, 9, 1]
+k = 2
+rotateArrayByDPlaceBetter(array,k)
 print(array)
