e5b914557af694d02193dc3abd827e51f8c28291
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-29 06:36:10 +0530

    best approach

--- Array/Maximum Consicutive Ones.py
+++ Array/Maximum Consicutive Ones.py
@@ -13,9 +13,20 @@ def findMaxCongOnesBruteForce(array):
             
     return maxConsicutive
         
+def findMaxCongOneBest(array):
+    maxConsicutive = 0
+    count = 0
+    for i in range(0,len(array)):
+        if(array[i] == 1):
+            count += 1
+        else:
+            count = 0
+        if ( maxConsicutive < count ):
+            maxConsicutive = count
+    return maxConsicutive
     
     
     
 
-array = [0,0,1,1,0,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,0,1,1,1,1,1]
-print(findMaxCongOnesBruteForce(array))
+array = [0,0,1,1,0,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,0,1,1,1,1,1]
+print(findMaxCongOneBest(array))
