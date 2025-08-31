d1fbbd500cc19e82f615e53aa91251b6de972f7f
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-31 18:04:06 +0530

    this will work only for non nagetive values

--- Array/Longest subarray with sum K.py
+++ Array/Longest subarray with sum K.py
@@ -1,4 +1,4 @@
-def longestSubarray(nums, k):
+def longestSubarrayBruteForce(nums, k):
     ans = 0
     for i in range(0,len(nums)-1):
         count = nums[i]
@@ -18,10 +18,25 @@ def longestSubarray(nums, k):
             
 
 
+def longestSubarrayBetter(nums,k):
+    preFixSum = {}
+    sum = 0
+    length = 0
+    for i in range(0,len(nums)):
+        sum+=nums[i]
+        if(sum == k):
+            length +=1
+        else:
+            if (sum - k) in preFixSum:
+              length = max([i - preFixSum[sum-k],length])
+              
+        preFixSum[sum] = i
+    return length
 
-nums = [10, 5, 2, 7, 1, 9]
-k=15
 
-# nums = [-1,1,1]
-# k = 1
-print(longestSubarray(nums,k))
+# nums = [10, 5, 2, 7, 1, 9]
+# k=15
+
+nums = [-1,1,1]
+k = 1
+print(longestSubarrayBetter(nums,k))
