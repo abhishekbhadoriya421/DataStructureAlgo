da627529d883e0c8af84e27ea3c51c3885e9890b
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-06 07:02:36 +0530

    optimized approach

--- Array/Maximum Sub Array Sum Kadanes Algo.py
+++ Array/Maximum Sub Array Sum Kadanes Algo.py
@@ -7,9 +7,18 @@ def bruteForce(array):
             if(continuousSum > maximumSubArraySum):
                 maximumSubArraySum = continuousSum
     return maximumSubArraySum
-            
+
+
+def optimizedApproach(array):
+    maximumSubArraySum = array[0]
+    summ = array[0]
+    for i in range(1,len(array)):
+        curSum = array[i] + summ
+        summ = max(curSum, array[i] )
+        maximumSubArraySum = max(summ,maximumSubArraySum)
+    return maximumSubArraySum
     
     
     
-array = [2, 3, -8, 7, -1, 2, 3]
-print(bruteForce(array))
+array = [-2,1,-3,4,-1,2,1,-5,4]
+print(optimizedApproach(array))
