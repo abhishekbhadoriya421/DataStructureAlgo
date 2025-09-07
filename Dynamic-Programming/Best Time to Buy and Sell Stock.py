f4e20fdb8e95b97817c45e4f4339ccfcbc4a0ac0
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-07 20:06:07 +0530

    optimized

--- Dynamic-Programming/Best Time to Buy and Sell Stock.py
+++ Dynamic-Programming/Best Time to Buy and Sell Stock.py
@@ -11,8 +11,25 @@ def bruteForce(prices):
             maximumProfit = profit
              
     return  maximumProfit if maximumProfit > 0 else 0
+
+def optimalAppraoch(prices):
+    maximumProfit = 0
     
+    sellingIndex = len(prices)-1
+    buyingIndex = sellingIndex -1
+    while(buyingIndex >= 0 and sellingIndex >=0):
+        currentProfit = prices[sellingIndex] - prices[buyingIndex]
+        if currentProfit > maximumProfit:
+            maximumProfit = currentProfit
+        if prices[sellingIndex] >= prices[buyingIndex]:
+            buyingIndex-=1
+        else:
+            sellingIndex = buyingIndex
+            buyingIndex-=1
+    
+    return maximumProfit
     
 
+# prices = [7,1,5,3,6,4]
 prices = [7,6,4,3,1]
-print(bruteForce(prices))
+print(optimalAppraoch(prices))
